ed28fe474aff5ff408e8cd299fb9c084e088e6b5
SepSol <SR-75@live.com>
2020-05-16 04:22:53 +0430

    displaying the name of the received item on the screen

--- src/screens/BlogDetailsScreen.js
+++ src/screens/BlogDetailsScreen.js
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { Context as BlogContext } from '../context/BlogContext';
+
 
 
 function BlogDetails({ route }) { // for passing data between screens
+
   const { id } = route.params; // for passing data between screens
+  const { state } = useContext(BlogContext);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
+  
   return(
     <View>
-      <Text>Blog Details Screen</Text>
-      <Text>{id}</Text>
+      <Text>{blogPost.title}</Text>
+      <Text>ID: {id}</Text>
     </View>
   );
 }
