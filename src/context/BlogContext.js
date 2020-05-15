9dee91e48e85bfe937c629b7e7de67a58463bff0
SepSol <SR-75@live.com>
2020-05-16 03:12:43 +0430

    Implemented blog post delete functionality

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -9,27 +9,27 @@ let idRangeArray = createRangeArray(10000);
 function blogReducer(state, action) {
   switch (action.type) {
     case 'add_blogpost':
-      return [ 
-        {id: getRandomUniqueNum(idRangeArray), title: `Blog Post #${state.length + 1}`},
-        ...state 
-      ];
+      return [ {id: getRandomUniqueNum(idRangeArray), title: `Blog Post #${state.length + 1}`}, ...state ];
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
   }
 }
 
 
-const addBlogPost = dispatch => () => dispatch({ type: 'add_blogpost' });
 // function addBlogPost(dispatch) {
 //   return(
 //     () => dispatch({ type: 'add_blogpost' })
 //   );
 // }
+const addBlogPost = dispatch => () => dispatch({ type: 'add_blogpost' });
+const deleteBlogPost = dispatch => (id) => dispatch({ type: 'delete_blogpost', payload: id });
 
 
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
